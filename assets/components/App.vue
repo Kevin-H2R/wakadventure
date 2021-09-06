5c37450cd462cd65326641950c462bf73499ea7a
Kevin <kevin.herrbach@avaent.co.kr>
2021-09-06 16:19:27 +0900

    First commit Symfony + encore + vue + vuetify

---
+++ assets/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>Hello world</v-row>
+        <v-row>
+          <v-btn color="primary">Click me</v-btn>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
