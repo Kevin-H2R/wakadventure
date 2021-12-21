39da7a5f8d8856670df2f69e1f61b3ed2214b4a7
Kevin <herrbach.kevin@gmail.com>
2021-12-21 18:28:48 +0900

    Starts adding user and database

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import {HttpModule} from "@nestjs/axios";
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import {User} from "./user/user.entity";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+      UserModule,
+      HttpModule,
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'wakaland',
+        entities: [User],
+        synchronize: true,
+      })
+  ],
   controllers: [AppController, AuthenticationController],
   providers: [AppService, AuthenticationService],
 })
---
+++ back/src/user/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    uid: string;
+
+    @Column()
+    access_token: string;
+
+    @Column()
+    expires_at: string;
+
+    @Column()
+    refresh_token: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
